ad53287ed3a8267dea083d6ba5585f9116ee3219
spawn000 <przemyslmatusiak@gmail.com>
2018-04-13 19:33:23 +0200

    kolejne poprawki bledow schematu

--- database/DB_schema.sql
+++ database/DB_schema.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `tbl_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_user` (
-  `user_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) DEFAULT NULL,
   `user_username` varchar(255) DEFAULT NULL,
   `user_password` varchar(255) DEFAULT NULL,
